a643f477db879687aa72e9ee4ed53df023cf1621
lakemonsters2635 <lakemonsters.software.team@gmail.com>
2017-10-30 20:28:53 -0700

    Something  happended heres the launcher

--- src/org/usfirst/frc/team2635/robot/subsystems/Launcher.java
+++ src/org/usfirst/frc/team2635/robot/subsystems/Launcher.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team2635.robot.subsystems;
 
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,10 +14,32 @@ public class Launcher extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	CANTalon flywheel0;
+	CANTalon flywheel1;
+	PIDController pid0;
+	PIDController pid1;
+	Encoder encoder0;
+	Encoder encoder1;
+	
+	public Launcher() {
+		flywheel0 = new CANTalon(0); //TODO: Set everything to real things
+		flywheel1 = new CANTalon(0);
+		
+		encoder0 = new Encoder(null, null);
+		encoder1 = new Encoder(null, null);
+		
+		pid0 = new PIDController(0, 0, 0, 0, null, flywheel0); 
+		pid1 = new PIDController(0, 0, 0, 0, null, flywheel1); 
+		
+		pid0.enable();
+		pid1.enable();
+	}
 	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+
+	
 }
 
